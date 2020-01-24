7e3be25329a30274c298a33911419b154e2fe622
val3rium <47543089+valerium-dev@users.noreply.github.com>
2020-01-24 17:25:24 -0600

    some commit

--- yash.c
+++ yash.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int cPID;
+    char *cmd;
+    while(1) {
+        cmd = readline("#");
+        cPID = fork();
+        if (cPID == 0) {
+             execvp();
+        }
+    }
 }
