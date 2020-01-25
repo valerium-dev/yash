e7187465cfc83ee0f1c53f56715683719a86484e
valerium-dev <admin@valerium.dev>
2020-01-24 19:47:12 -0600

    added tokenizeString prototype

--- yash.c
+++ yash.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <readline/readline.h>
 #include <unistd.h>
 
+char ** tokenizeString(char * string);
+
 int main() {
     int cPID;
-    char* cmd;
+    char * cmd;
+    char ** tokens;
+
     while(1) {
 	cmd = readline("# ");
+	tokens = tokenizeString(cmd);
         cPID = fork();
         if (cPID == 0) {
             execvp(cmd, cmd);
 	}
     }
+}
+
+char ** tokenizeString(char * string) {
+    char ** tokens = malloc(sizeof(string));
+    char currentChar;
+
+    while(currentChar != '\n') {
+        currentChar = *string;
+    }
+
+    return tokens;    
 }
